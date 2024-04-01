75e1ff1a1ed81215d5cb8199c449d072ec1d2d34
VIVEK <shsrna41@gmail.com>
2024-04-01 10:23:43 +0530

    ggg

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,63 @@
 import { useState } from "react";
 
+const API_BASE_URL = 'https://api.met.no/weatherapi/locationforecast/2.0/compact'
+
 export default function Home() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [weatherForcastData, setWeatherForcastData] = useState([]);
+  async function getWeatherData(e){
+    e.preventDefault();
+    try{
+      const response = await fetch(`${API_BASE_URL}?lat=${latitude}&lon=${longitude}`);
+      const weatherData = await response.json();
+      setWeatherForcastData(weatherData?.properties?.timeseries?.slice(0,30))
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
   return (
     <>
-      <div id="root"></div>
+      <div id="root">
+        <h1>Weather Forecast</h1>
+        <form onSubmit={getWeatherData}>
+          <label>Latitude:
+          <input type="text" className="latitude" value={latitude} onChange={(e)=>setLatitude(e.target.value)}/>
+
+
+          </label>
+
+          <label>Longitude:
+          <input type="text" className="longitude" value={longitude} onChange={(e)=>setLongitude(e.target.value)}/>
+
+
+          </label>
+
+          <button type="submit">Get Forecast</button>
+        </form>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Time</th>
+            <th>Temperature (°C)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {weatherForcastData.map((weatherEntry)=>{
+            return(
+              <tr key={weatherEntry.time}>
+                <td>{new Date(weatherEntry.time).toLocaleString()}</td>
+                <td>{weatherEntry?.data?.instant?.details?.air_temperature.toFixed(1)}</td>
+
+                <td>{weatherEntry?.data?.next_1_hours?.summary?.symbol_code}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
